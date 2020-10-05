37b689c7c77d3398d06914fd0532e969f8b8ec38
titanium <nkansahisaac41@gmail.com>
2020-10-05 04:12:57 +0000

    done with module 4

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from "../product.service";
   styleUrls: ['./product-tracklisting.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
-  private albumInfo: Album;
+  albumInfo: Album;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
